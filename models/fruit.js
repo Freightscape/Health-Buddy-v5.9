552f6d02b6c56e614092172c9ea65610cf6ca193
hnhtran <tran_eetas@yahoo.com>
2022-06-08 23:42:11 +1000

    preparing MVC model

---
+++ models/fruit.js
@@ -0,0 +1,29 @@
+/////////////////////////////////////////////
+// Import Our Dependencies
+/////////////////////////////////////////////
+const mongoose = require("./connection");
+
+////////////////////////////////////////////////
+// Our Models
+////////////////////////////////////////////////
+// pull schema and model from mongoose
+const { Schema, model } = mongoose;
+// upper line is combined of these 2 lines
+// const Schema = mongoose.Schema
+// const model = mogoose.model
+
+// make fruis schema
+const fruitsSchema = new Schema({
+  name: String,
+  color: String,
+  readyToEat: Boolean,
+});
+
+// make fruit model
+const Fruit = model("Fruit", fruitsSchema);
+
+
+///////////////////////////////////////////////////
+// Export Model
+///////////////////////////////////////////////////
+module.exports = Fruit
--- server.js
+++ server.js
@@ -1,51 +1,11 @@
 /////////////////////////////////////////////
 // Import Our Dependencies
 /////////////////////////////////////////////
-require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const methodOverride = require("method-override");
-const mongoose = require("mongoose");
 const path = require("path");
-const { stringify } = require("querystring");
-
-/////////////////////////////////////////////
-// Database Connection
-/////////////////////////////////////////////
-// Setup inputs for our connect function
-const DATABASE_URL = process.env.DATABASE_URL;
-const CONFIG = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
-
-// Establish Connection
-mongoose.connect(DATABASE_URL, CONFIG);
-
-// Events for when connection opens/disconnects/errors
-mongoose.connection
-  .on("open", () => console.log("Connected to Mongoose"))
-  .on("close", () => console.log("Disconnected from Mongoose"))
-  .on("error", (error) => console.log(error));
-
-////////////////////////////////////////////////
-// Our Models
-////////////////////////////////////////////////
-// pull schema and model from mongoose
-const { Schema, model } = mongoose;
-// upper line is combined of these 2 lines
-// const Schema = mongoose.Schema
-// const model = mogoose.model
-
-// make fruis schema
-const fruitsSchema = new Schema({
-  name: String,
-  color: String,
-  readyToEat: Boolean,
-});
-
-// make fruit model
-const Fruit = model("Fruit", fruitsSchema);
+const Fruit = require('./models/fruit')
 
 /////////////////////////////////////////////////
 // Create our Express Application Object Bind Liquid Templating Engine
